0ac145f65011e32c07a04db130d2e904aaf78869
Andrei <aalisenak@gmail.com>
2020-01-21 09:35:40 +0300

    Updates

---
+++ precache-manifest.3870f7cf215dbd3504231482e046d0a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00d9217e6e674c9a4fa4ac765f44dac4",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "a383a8c5409255ec9518",
+    "url": "/todo/static/css/main.00a3ae9e.chunk.css"
+  },
+  {
+    "revision": "532a3c6b7a2b83751e6b",
+    "url": "/todo/static/js/2.8e9e2458.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/todo/static/js/2.8e9e2458.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a383a8c5409255ec9518",
+    "url": "/todo/static/js/main.9b98d2c7.chunk.js"
+  },
+  {
+    "revision": "dd9adfdb09f83cb1a7aa",
+    "url": "/todo/static/js/runtime-main.f7a45fe9.js"
+  }
+]);
